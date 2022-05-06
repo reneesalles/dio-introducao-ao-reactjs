3c795eca6384ee03ea757197796476f146b8df80
reneesalles <reneesalles@hotmail.com>
2022-05-06 18:11:57 -0300

    Update Lição-01
    Novo sub-módulo Configurações

--- lesson-01/README.md
+++ lesson-01/README.md
@@ -1,1 +1,5 @@
 # DIO - Introdução ao ReactJS - Lição 01
+
+## Sub-Módulos
+
+1. [Configurações](./configuration/)
---
+++ lesson-01/configuration/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const App = () => {
+    return (
+        <div className='App'>
+            Olá Mundo!!
+        </div>
+    )
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
