1ecef1e7c0bf20a7ad3d25e4463cd807379f9123
reneesalles <reneesalles@hotmail.com>
2022-05-09 20:54:49 +0000

    Refatorando métodos Render
    Antes eram Arrow Funcions, agora são apenas variáveis

--- lesson-03/list-and-keys/src/App.jsx
+++ lesson-03/list-and-keys/src/App.jsx
@@ -1,28 +1,24 @@
 import React, { useState } from 'react';
 
-let hasCustomer = false;
+let hasCustomer = true;
 const buttonCustomer = <button>Histórico de Clientes</button>;
 const buttonNewCustomer = <button>Cadastrar novo Cliente</button>
 
 const App = () => {
-    const renderBtnCustomer = () =>
+    const renderBtnCustomer =
         <div>
             {buttonCustomer}
         </div>;
 
-    const renderBtnNewCustomer = () =>
+    const renderBtnNewCustomer =
         <div>
             {buttonNewCustomer}
         </div>;
 
-    const renderNull = () => {
-        if (!hasCustomer) return null;
-        return (
-            <div>
-                <h3>Cliente: Renêe Sales</h3>
-            </div>
-        )
-    }
+    const renderNull = !hasCustomer ? null :
+        <div>
+            <h3>Cliente: Renêe Sales</h3>
+        </div>;
 
     return (
         <div>
@@ -30,15 +26,15 @@ const App = () => {
             <hr />
 
             <h5>Com condicional &&</h5>
-            {hasCustomer && renderBtnCustomer()}
+            {hasCustomer && renderBtnCustomer}
             <hr />
 
             <h5>Com ternário (if/else)</h5>
-            {hasCustomer ? renderBtnCustomer() : renderBtnNewCustomer()}
+            {hasCustomer ? renderBtnCustomer : renderBtnNewCustomer}
             <hr />
 
             <h5>Opcional</h5>
-            {renderNull()}
+            {renderNull}
         </div>
     );
 };
