77d2a763e622ac970fd6d731f384ba4a74a831cb
reneesalles <reneesalles@hotmail.com>
2022-05-09 21:24:36 +0000

    Finalizando o Sub-Módulo - Listas e Chaves

--- lesson-03/list-and-keys/src/App.jsx
+++ lesson-03/list-and-keys/src/App.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
 
+const customerList = [
+    {
+        id: 1,
+        name: 'Renêe Sales',
+        skills: [
+            'React', 'C#', 'Flutter', 'Bootstrap', 'JQuery'
+        ],
+    },
+    {
+        id: 2,
+        name: 'Stefani Sales',
+        skills: [
+            'Angular', 'Ionic', 'HTML', 'CSS', 'Bootstrap'
+        ],
+    },
+    {
+        id: 3,
+        name: 'Aroldo Sales',
+        skills: [
+            'Bancos de Dados', 'Oracle', 'BI',
+        ],
+    },
+    {
+        id: 4,
+        name: 'Fulano Dital'
+    },
+];
+
 const App = () => {
+    const renderCustomers = (customer, index) => {
+        return <li key={`customer-${index}`}>
+            <div>
+                <b>{customer.id}: {customer.name}</b>
+            </div>
+            {customer.skills && (<div>
+                <b>Skills:</b>
+                <ul>
+                    {customer.skills.map(renderSkills)}
+                </ul>
+            </div>)}
+            <br/>
+        </li>
+    };
+    
+    const renderSkills = (skill, index) => {
+        return <li key={`skill-${index}`}>{skill}</li>;
+    }
+    
     return (
         <div>
-            <p>Digital Innovation One</p>
+            <h1>Listas e Chaves</h1>
+            <div>
+                <h2>Customers:</h2>
+                <ul>
+                    {customerList.map(renderCustomers)}
+                </ul>
+            </div>
         </div>
     );
 };
