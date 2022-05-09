11a72b2d47a5dc49bcfb63cb3a8a8fab25ab6591
reneesalles <reneesalles@hotmail.com>
2022-05-09 20:58:40 +0000

    Limpando o arquivo App.jsx para estado inicial

--- lesson-03/list-and-keys/src/App.jsx
+++ lesson-03/list-and-keys/src/App.jsx
@@ -1,40 +1,9 @@
-import React, { useState } from 'react';
-
-let hasCustomer = true;
-const buttonCustomer = <button>Histórico de Clientes</button>;
-const buttonNewCustomer = <button>Cadastrar novo Cliente</button>
+import React from 'react';
 
 const App = () => {
-    const renderBtnCustomer =
-        <div>
-            {buttonCustomer}
-        </div>;
-
-    const renderBtnNewCustomer =
-        <div>
-            {buttonNewCustomer}
-        </div>;
-
-    const renderNull = !hasCustomer ? null :
-        <div>
-            <h3>Cliente: Renêe Sales</h3>
-        </div>;
-
     return (
         <div>
-            <p>Renderizando Elementos</p>
-            <hr />
-
-            <h5>Com condicional &&</h5>
-            {hasCustomer && renderBtnCustomer}
-            <hr />
-
-            <h5>Com ternário (if/else)</h5>
-            {hasCustomer ? renderBtnCustomer : renderBtnNewCustomer}
-            <hr />
-
-            <h5>Opcional</h5>
-            {renderNull}
+            <p>Digital Innovation One</p>
         </div>
     );
 };
